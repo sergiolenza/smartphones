cfd39773fbe6d4ddbcfcb488e759be2430d21306
Sergio Lenza <slenzamori.dev@gmail.com>
2022-04-13 16:05:21 +0200

    [ProductDetails] improve ui

--- src/infrastructure/ui/components/ProductDetails/ProductDetails.js
+++ src/infrastructure/ui/components/ProductDetails/ProductDetails.js
@@ -1,6 +1,71 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 import useProduct from '../../hooks/useProduct';
+
+const ProductDetail = styled.div`
+  display: flex;
+  max-width: 700px;
+  align-items: center;
+  justify-content: space-around;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: white;
+  padding: 2rem;
+`;
+
+const ProductDetailImage = styled.img`
+  height: 100%;
+`;
+
+const ProductDetailDescriptionParagraph = styled.p`
+  margin-bottom: 8px;
+`;
+
+const ProductDetailDescriptionKey = styled.span`
+  font-weight: 800;
+`;
+
+const ProductDetailActions = styled.div`
+  padding-top: 1rem;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Dropdown = styled.select`
+  outline: none;
+  padding: 7px;
+  border-radius: 6px;
+  font-size: 16px;
+  background: #fbfbfb;
+  border: 2px solid #ff4742;
+  border-radius: 4px;
+  color: #ff4742;
+  font-weight: 800;
+  :focus {
+    border: 2px solid #ff4742;
+  }
+`;
+
+const AddToCartButton = styled.button`
+  display: inline-block;
+  outline: 0;
+  cursor: pointer;
+  border-radius: 2px;
+  border: 2px solid #ff4742;
+  border-radius: 4px;
+  color: #ff4742;
+  background: 0 0;
+  padding: 8px;
+  box-shadow: rgba(0, 0, 0, 0.07) 0px 2px 4px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1.5px 0px;
+  font-weight: 800;
+  font-size: 16px;
+  height: 42px;
+  :hover {
+    background-color: #ff4742;
+    color: #fff;
+  }
+`;
 
 const ProductDetails = ({ onAddToCart }) => {
   const params = useParams();
@@ -16,6 +81,7 @@ const ProductDetails = ({ onAddToCart }) => {
   }
 
   const {
+    imgUrl,
     brand,
     model,
     price,
@@ -32,39 +98,76 @@ const ProductDetails = ({ onAddToCart }) => {
   const { colors, storages } = options;
 
   return (
-    <div>
-      <p>Brand: {brand}</p>
-      <p>Model: {model}</p>
-      <p>Price: {price} €</p>
-      <p>CPU: {cpu}</p>
-      <p>RAM: {ram}</p>
-      <p>Operative system: {os}</p>
-      <p>Display resolution: {displayResolution}</p>
-      <p>Battery: {battery}</p>
-      <p>Dimensions: {dimentions}</p>
-      <p>Weight: {weight} gr</p>
-      <select name="colors">
-        {colors.map(({ code, name }) => {
-          return (
-            <option key={code} value={code}>
-              {name}
-            </option>
-          );
-        })}
-      </select>
-      <select name="storages">
-        {storages.map(({ code, name }) => {
-          return (
-            <option key={code} value={code}>
-              {name}
-            </option>
-          );
-        })}
-      </select>
-      <button type="button" onClick={onClickAddToCart}>
-        Add to cart
-      </button>
-    </div>
+    <ProductDetail>
+      <ProductDetailImage src={imgUrl} />
+      <div>
+        <div>
+          <ProductDetailDescriptionParagraph>
+            <ProductDetailDescriptionKey>Brand: </ProductDetailDescriptionKey>
+            {brand}
+          </ProductDetailDescriptionParagraph>
+          <ProductDetailDescriptionParagraph>
+            <ProductDetailDescriptionKey>Model: </ProductDetailDescriptionKey>
+            {model}
+          </ProductDetailDescriptionParagraph>
+          <ProductDetailDescriptionParagraph>
+            <ProductDetailDescriptionKey>Price: </ProductDetailDescriptionKey>
+            {price} €
+          </ProductDetailDescriptionParagraph>
+          <ProductDetailDescriptionParagraph>
+            <ProductDetailDescriptionKey>CPU: </ProductDetailDescriptionKey>
+            {cpu}
+          </ProductDetailDescriptionParagraph>
+          <ProductDetailDescriptionParagraph>
+            <ProductDetailDescriptionKey>RAM: </ProductDetailDescriptionKey>
+            {ram}
+          </ProductDetailDescriptionParagraph>
+          <ProductDetailDescriptionParagraph>
+            <ProductDetailDescriptionKey>Operative system: </ProductDetailDescriptionKey>
+            {os}
+          </ProductDetailDescriptionParagraph>
+          <ProductDetailDescriptionParagraph>
+            <ProductDetailDescriptionKey>Display resolution: </ProductDetailDescriptionKey>
+            {displayResolution}
+          </ProductDetailDescriptionParagraph>
+          <ProductDetailDescriptionParagraph>
+            <ProductDetailDescriptionKey>Battery: </ProductDetailDescriptionKey>
+            {battery}
+          </ProductDetailDescriptionParagraph>
+          <ProductDetailDescriptionParagraph>
+            <ProductDetailDescriptionKey>Dimensions: </ProductDetailDescriptionKey>
+            {dimentions}
+          </ProductDetailDescriptionParagraph>
+          <ProductDetailDescriptionParagraph>
+            <ProductDetailDescriptionKey>Weight: </ProductDetailDescriptionKey>
+            {weight} gr
+          </ProductDetailDescriptionParagraph>
+        </div>
+        <ProductDetailActions>
+          <Dropdown name="colors">
+            {colors.map(({ code, name }) => {
+              return (
+                <option key={code} value={code}>
+                  {name}
+                </option>
+              );
+            })}
+          </Dropdown>
+          <Dropdown name="storages">
+            {storages.map(({ code, name }) => {
+              return (
+                <option key={code} value={code}>
+                  {name}
+                </option>
+              );
+            })}
+          </Dropdown>
+          <AddToCartButton type="button" onClick={onClickAddToCart}>
+            Add to cart
+          </AddToCartButton>
+        </ProductDetailActions>
+      </div>
+    </ProductDetail>
   );
 };
 
